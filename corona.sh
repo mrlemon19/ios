a8911be453a4235cb38c1fbe33d27c4d62ed67c1
mrlemon19 <93056957+mrlemon19@users.noreply.github.com>
2022-03-25 14:28:14 +0100

    Add files via upload

---
+++ corona.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+# prvni projekt do ios 
+# vytvoril: Jakub Lukas aka xlukas18
+
+NUM1=3
+NUM2=5
+
+if [ "$NUM1" == "$NUM2" ]
+then
+	echo "nums are eqal!"
+else
+	echo "nums are not eqval!"
+fi
