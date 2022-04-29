8974e04f0a16555c1e4791278dc88119139906d7
mrlemon19 <93056957+mrlemon19@users.noreply.github.com>
2022-04-29 19:46:51 +0200

    Add files via upload

--- proj2.c
+++ proj2.c
@@ -9,19 +9,26 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <time.h>
 
 #define MMAP(pointer) {(pointer) = mmap(NULL, sizeof(*(pointer)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);}
 #define UNMAP(pointer) {munmap((pointer), sizeof((pointer)));}
 
-// prekladat takto:     cc -std=gnu99 -Wall -Wextra -Werror -pedantic proj2.c -o proj2
+// prekladat takto:     cc -std=gnu99 -pthread -Wall -Wextra -Werror -pedantic proj2.c -o proj2
 
-//FILE *pfile;
+FILE *pfile;
 int *hy = NULL;
 int *ox = NULL;
-int *printnum = 1;
-sem_t *qvGen = NULL;
+int *printnum = NULL;
+int *molnum = NULL;
+int *count = NULL;
+sem_t *mutex = NULL;
 sem_t *qvHy = NULL;
 sem_t *qvOx = NULL;
+sem_t *printGrd = NULL;
+sem_t *barrier1 = NULL;
+sem_t *barrier2 = NULL;
+sem_t *barMutex = NULL;
 
 
 // zatim hlasi error
@@ -29,29 +36,48 @@ sem_t *qvOx = NULL;
 
 int init()
 {
-    //pfile = fopen("proj2.out", "w");
+    pfile = fopen("proj2.out", "w");
     MMAP(hy);
     MMAP(ox);
     MMAP(printnum);
-    if ((qvGen = sem_open("/xlukas18.ios.proj2.sem.qvgen", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED) return -1;
-    else{return 0;}
-    if ((qvHy = sem_open("/xlukas18.ios.proj2.sem.qvhy", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) return -1;
-    else{return 0;}
-    if ((qvOx = sem_open("/xlukas18.ios.proj2.sem.qvox", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) return -1;
-    else{return 0;}
+    MMAP(molnum);
+    MMAP(count);
+    *printnum = 1;
+    *molnum = 1;
+    *hy = 0;
+    *ox = 0;
+    *count = 0;
+    if ((mutex = sem_open("/xlukas18.sem.mutex", O_CREAT, 0666, 1)) == SEM_FAILED) return -1;
+    else if ((qvHy = sem_open("/xlukas18.sem.qvhy", O_CREAT, 0666, 0)) == SEM_FAILED) return -1;
+    else if ((qvOx = sem_open("/xlukas18.sem.qvox", O_CREAT, 0666, 0)) == SEM_FAILED) return -1;
+    else if ((printGrd = sem_open("/xlukas18.sem.printgrd", O_CREAT, 0666, 1)) == SEM_FAILED) return -1;
+    else if ((barrier1 = sem_open("/xlukas18.sem.barrier1", O_CREAT, 0666, 0)) == SEM_FAILED) return -1;
+    else if ((barrier2 = sem_open("/xlukas18.sem.barrier2", O_CREAT, 0666, 0)) == SEM_FAILED) return -1;
+    else if ((barMutex = sem_open("/xlukas18.sem.barmutex", O_CREAT, 0666, 1)) == SEM_FAILED) return -1;
+    else return 0;
 }
 void clean()
 {
     UNMAP(hy);
     UNMAP(ox);
     UNMAP(printnum);
-    sem_close(qvGen);
-    sem_unlink("/xlukas18.ios.proj2.sem.qvgen");
+    UNMAP(molnum);
+    UNMAP(count);
+    sem_close(mutex);
+    sem_unlink("/xlukas18.sem.mutex");
     sem_close(qvHy);
-    sem_unlink("/xlukas18.ios.proj2.sem.qvhy");
+    sem_unlink("/xlukas18.sem.qvhy");
     sem_close(qvOx);
-    sem_unlink("/xlukas18.ios.proj2.sem.qvox");
-    //if (pfile != NULL) fclose(pfile);
+    sem_unlink("/xlukas18.sem.qvox");
+    sem_close(printGrd);
+    sem_unlink("/xlukas18.sem.printgrd");
+    sem_close(barrier1);
+    sem_unlink("/xlukas18.sem.barrier1");
+    sem_close(barrier2);
+    sem_unlink("/xlukas18.sem.barrier2");
+    sem_close(barMutex);
+    sem_unlink("/xlukas18.sem.barmutex");
+    if (pfile != NULL) fclose(pfile);
 }
 
 
@@ -63,47 +89,147 @@ void raiseErr()
 void processOx(int id, int dell1, int dell2)
 {
     id++;
-    // srandom pro sleeptime1 a sleeptime2
-    // int sleeptime1 = (rand() % dell1 + 1);   // FIXME dedelat random generovani zpozdeni
-    // int sleeptime2 = (rand() % dell2 + 1); // same ^^
-    printf("O %d: started \n", id);
-    usleep(dell1); // nefunguje jeste, predelat na sleeptime2
-    sem_wait(qvGen); //defaultne na 1
-    ox++;
+    srand(id * time(0));
+    int sleeptime1 = (rand() % dell1 + 1);
+    int sleeptime2 = (rand() % dell2 + 1);
+    
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: O %d: started\n",*printnum, id);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+    
+    usleep(sleeptime1); 
+    
+    sem_wait(mutex);    // defaultne na 1
+    (*ox)++;
     if (*hy >= 2) {
         sem_post(qvHy);
         sem_post(qvHy);
-        hy -= 2;
+        (*hy) -= 2;
         sem_post(qvOx);
-        ox --;
+        (*ox)--;
         }
-    else {sem_post(qvGen);}
-    printf("O %d: going to queue \n", id);
+    else sem_post(mutex);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: O %d: going to queue\n",*printnum, id);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
     sem_wait(qvOx); //defaultne na 0
-    printf("O %d: creating molecule [cislo molekuly] \n", id); // FIXME cislo molekuly
-    usleep(dell2); // nefunguje jeste, predelat na sleeptime2
-    printf("O %d: molecule [cislo melekuly] created \n", id);
-    // qvOx.post()
-    // qvGen.post()
+
+    sem_wait(printGrd);     // vypise, ze tvori molekulu
+        fprintf(pfile, "%d: O %d: creating molecule %d\n",*printnum, id, *molnum);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
+    sem_wait(barMutex);
+    (*count)++;    // pocet procesu tvoricich molekulu
+    if (*count == 3){
+        sem_post(barrier1);
+        sem_post(barrier1);
+        sem_post(barrier1);
+    }
+    else sem_post(barMutex);
+
+    sem_wait(barrier1);
+
+    usleep(sleeptime2);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: O %d: molecule %d created\n",*printnum, id, *molnum);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
+    (*count)--;
+    if (*count == 0){
+        (*molnum)++;
+        sem_post(barrier2);
+        sem_post(barrier2);
+        sem_post(barrier2);
+        sem_post(barMutex);
+        sem_post(mutex);    
+    }
+    sem_wait(barrier2);
     exit(0);
 }
 
 void processHy(int id, int dell1, int dell2)
 {
     id++;
-    //int sleeptime1 = (rand() % dell1 + 1);   // FIXME dedelat random generovani zpozdeni
-    //int sleeptime2 = (rand() % dell2) // same ^^
-    printf("H %d: started \n", id);
-    usleep(dell1); // nefunguje jeste, predelat na sleeptime1
-    // sdilena pamet: hydrogens++
-    // if (hydrogen >= 2) {}
-    // semH.wait() - je defaultne nastaven na 0
-    printf("H %d: going to queue \n", id);
-    printf("H %d: creating molecule [cislo molekuly] \n", id); // FIXME cislo molekuly
-    // sdilena pamet: hydrogens--
-    usleep(dell2); // nefunguje jeste, predelat na sleeptime2
-    printf("H %d: molecule [cislo melekuly] created \n", id);
-    // semH.post(2)
+    srand(id * time(0));
+    int sleeptime1 = (rand() % dell1 + 1);
+    int sleeptime2 = (rand() % dell2 + 1);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: H %d: started\n",*printnum, id);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+    usleep(sleeptime1);
+
+    sem_wait(mutex);
+    (*hy)++;      // hy++
+    if (*hy >= 2 && *ox >= 1) {
+        sem_post(qvHy);
+        sem_post(qvHy);
+        (*hy) -= 2;
+        sem_post(qvOx);
+        (*ox)--;
+        }
+    else sem_post(mutex);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: H %d: going to queue\n",*printnum, id);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
+    sem_wait(qvHy);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: H %d: creating molecule %d\n",*printnum, id, *molnum);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
+    // barrier in some sort
+    sem_wait(barMutex);
+    
+    (*count)++;    // pocet procesu tvoricich molekulu
+    if (*count == 3){
+        sem_post(barrier1);
+        sem_post(barrier1);
+        sem_post(barrier1);
+    }
+    else sem_post(barMutex);
+
+    sem_wait(barrier1);
+
+    usleep(sleeptime2);
+
+    sem_wait(printGrd);
+        fprintf(pfile, "%d: H %d: molecule %d created\n",*printnum, id, *molnum);
+        fflush(pfile);
+        (*printnum)++;
+    sem_post(printGrd);
+
+    (*count)--;
+    if (*count == 0){
+        (*molnum)++;
+        sem_post(barrier2);
+        sem_post(barrier2);
+        sem_post(barrier2);
+        sem_post(barMutex);
+        sem_post(mutex);    
+    }
+
+    sem_wait(barrier2);
+
     exit(0);
 }
 
@@ -112,7 +238,6 @@ void genO(int nox, int dell1, int dell2)
     for (int i = 0; i < nox; i++){
         pid_t ox = fork();
         if (ox == 0){
-            printf("ox num. %d generating\n", i);
             processOx(i, dell1, dell2);
         }
     }
@@ -124,7 +249,6 @@ void genH(int nhy, int dell1, int dell2)
     for (int i = 0; i < nhy; i++){
         pid_t hy = fork();
         if (hy == 0){
-            printf("hy num. %d generating\n", i);
             processHy(i, dell1, dell2);
         }
     }
@@ -162,13 +286,10 @@ int main(int argc, char **argv)
  // checks if input data are in right format
     if (inputSort(argv)) return 1;
     
-    // zatim nefunguje
-    /*
     if (init() == -1){
         clean();
         return -1;
     }
-    */
     
     int NH = atoi(argv[2]);
     int NO = atoi(argv[1]);
@@ -187,7 +308,7 @@ int main(int argc, char **argv)
     else if (hlavni == 0)
     {
         pid_t OH = fork();
-        pid_t wpid2;    // pro cekani procesu dokud nejsou vnorene procesy ukonceny
+        //pid_t wpid2;    // pro cekani procesu dokud nejsou vnorene procesy ukonceny
 
         if (OH < 0)     // kontrola chyby forku
         {
@@ -200,13 +321,13 @@ int main(int argc, char **argv)
         else{
             genH(NH, delay1, delay2);   // child proces 2 generuje vodik
         }
-        while ((wpid2 = wait(&status)) > 0);    // cekani...
+        //while ((wpid2 = wait(&status)) > 0);    // cekani...
         
     }
 
     while ((wpid = wait(&status)) > 0);         // cekani...
 
     exit(0);
-    //clean(); // uklizeci funkce
+    clean(); // uklizeci funkce
     return 0;
 }
